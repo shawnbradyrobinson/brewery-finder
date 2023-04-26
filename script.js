803662335e51838179b420a26d7f4b6b57ba38c7
shawnbradyrobinson <shawnbradyrobinson1@gmail.com>
2023-04-25 19:36:48 -0500

    built out 'searchByCity' function that works if you pass it an explicit string...data pops onto console log

--- script.js
+++ script.js
@@ -3,49 +3,58 @@ var breweryURL = "https://api.openbrewerydb.org/v1/breweries";
 var byCityURL = "https://api.openbrewerydb.org/v1/breweries?by_city=san_diego&per_page=10";
 var byTypeURL = "https://api.openbrewerydb.org/v1/breweries?by_type=micro&per_page=10";
 
-fetch (breweryURL)
-.then(function (response){
+// fetch (breweryURL)
+// .then(function (response){
 
-return response.json();
+// return response.json();
 
-})
-.then (function (data){
+// })
+// .then (function (data){
 
-console.log(data);
+// console.log(data);
 
-})
+// })
 
-fetch (byCityURL)
-.then(function (response){
+// fetch (byTypeURL)
+// .then(function (response){
 
-return response.json();
+// return response.json();
 
-})
-.then (function (data){
+// })
+// .then (function (data){
+// // vv this is the basic format to access data out vv 
+// console.log(data[0].address_1);
 
-for (i=0; i<data.length; i++){
-    console.log(data);
-    console.log("--------------");
-    console.log("BREWERY NAME: " +data[i].name);
-    console.log("ADDRESS: " +data[i].address_1); 
-    console.log("CITY: " +data[i].city);
-    console.log("LATITUDE: " +data[i].latitude);
-    console.log("LONGITUDE: " +data[i].longitude);
-    console.log("");
-    console.log("----------------");
+// })
 
+
+function searchByCity(cityString){
+    var byCityURL = "https://api.openbrewerydb.org/v1/breweries?by_city="+cityString+"&per_page=10";
+    
+
+    fetch (byCityURL)
+    .then(function (response){
+    
+    return response.json();
+    
+    })
+    .then (function (data){
+    
+    for (i=0; i<data.length; i++){
+        console.log(data);
+        console.log("--------------");
+        console.log("BREWERY NAME: " +data[i].name);
+        console.log("ADDRESS: " +data[i].address_1); 
+        console.log("CITY: " +data[i].city);
+        console.log("LATITUDE: " +data[i].latitude);
+        console.log("LONGITUDE: " +data[i].longitude);
+        console.log("");
+        console.log("----------------");
+    
+    }
+    
+    })
+    
 }
 
-})
-
-fetch (byTypeURL)
-.then(function (response){
-
-return response.json();
-
-})
-.then (function (data){
-// vv this is the basic format to access data out vv 
-console.log(data[0].address_1);
-
-})
+searchByCity("San Diego");
